6ddba723f06f8a48877a5f14ec65db3f9f8e558c
badgerttl <jmorris@badger-state.net>
2018-01-29 02:15:21 +0000

    added basic apache cookbook

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,3 +3,16 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+#
+#Install apache package
+package 'apache2' do
+	package_name 'httpd'
+	action :install
+end
+
+#Start and enable apache service
+service 'apache2' do
+	service_name 'httpd'
+	action [:start, :enable]
+end
+
