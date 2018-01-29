418aa3b0cd088246de6f58f2e7a6d774b0af7091
badgerttl <jmorris@badger-state.net>
2018-01-29 03:03:25 +0000

    create motd with hostname

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -5,8 +5,15 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 #
 #Install apache package
+
+if node['platform_family'] == "rhel"
+	package = "httpd"
+elsif node['platform_family'] == "apache2"
+	package = "apache2"
+end
+
 package 'apache2' do
-	package_name 'httpd'
+	package_name package
 	action :install
 end
 
@@ -16,3 +23,4 @@ service 'apache2' do
 	action [:start, :enable]
 end
 
+#include_recipe 'apache::websites'
